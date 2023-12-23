fc809cca4e5c3f4d97a52086cd1f560ad24dcca0
licia-maps <152826750+licia-maps@users.noreply.github.com>
2023-12-23 20:15:05 +0200

    daily intake types
    Record the number of animal intakes made per day (of October), per intake type.

---
+++ october_intaketypes.sql
@@ -0,0 +1,13 @@
+select
+	IF(substr(date,4,2) LIKE "%/%", LEFT(substr(date,4,2),1) , substr(date,4,2)) AS DayOfOctober,
+    sum(case when IntakeType = "Stray" then 1 else 0 end) as "Stray",
+    sum(case when IntakeType = "Owner Surrender" then 1 else 0 end) as "Owner Surrender",
+    sum(case when IntakeType = "Wildlife" then 1 else 0 end) as "Wildlife",
+    sum(case when IntakeType = "Public Assist" then 1 else 0 end) as "Public Assist",
+    sum(case when IntakeType = "Euthanasia Request" then 1 else 0 end) as "Euthanasia Request"
+from
+	intakes
+group by
+	DayOfOctober
+order by
+	DayOfOctober asc;
