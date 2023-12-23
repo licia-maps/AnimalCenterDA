400809270c9876a0d557ffafac8914ec6f4f0283
licia-maps <152826750+licia-maps@users.noreply.github.com>
2023-12-23 20:51:02 +0200

    Update age.sql

--- age.sql
+++ age.sql
@@ -1,12 +1,16 @@
+-- File: age.sql
+
+-- Convert the age of each animal into years to make it easier to make comparisons 
+-- about how age influences addmition into the centre.
 SELECT
 	AnimalID,
     ROUND(CASE
     WHEN
 		AgeUponIntake LIKE "%year%" THEN left(AgeUponIntake,2)
     WHEN 
-		AgeUponIntake like "%week%" THEN left(AgeUponIntake,2)/52
+		AgeUponIntake like "%week%" THEN left(AgeUponIntake,2)/52 -- there are 52 weeks in a year
     ELSE
-		left(AgeUponIntake,2)/12
+		left(AgeUponIntake,2)/12 -- there are 12 months in a year
 	END,2) AS AgeInYears
 from
 	intakes;
