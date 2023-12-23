a4a76386eaa33fd492c5e676efefa220a2e8f17c
licia-maps <152826750+licia-maps@users.noreply.github.com>
2023-12-23 20:47:00 +0200

    Update october_intakes.sql
    Add comments to october_intakes.sql

--- october_intakes.sql
+++ october_intakes.sql
@@ -1,3 +1,6 @@
+-- File: october_intakes.sql
+
+-- the number of animals taken in each day of october per intake type
 select
 	IF(substr(date,4,2) LIKE "%/%", LEFT(substr(date,4,2),1) , substr(date,4,2)) AS DayOfOctober,
     sum(case when IntakeType = "Stray" then 1 else 0 end) as "Stray",
@@ -11,3 +14,4 @@ group by
 	DayOfOctober
 order by
 	DayOfOctober asc;
+    
