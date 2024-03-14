3fc692ed09c291530c20ae81715e90017f6e8e73
Alicia Maposa <152826750+licia-maps@users.noreply.github.com>
2024-03-14 22:27:35 +0200

    Delete sql_scripts/breeds.sql

--- sql_scripts/breeds.sql
+++
@@ -1,9 +0,0 @@
--- File: breeds
-
--- Figure out the total makeup of each breed in the centre
-select
-	Breed,
-    Color,
-    count(Color) over (partition by Breed) as TotalNumber
-from
-	intakes;
